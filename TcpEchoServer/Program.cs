3ebfe824dace0ce60119fcee2fc2fae2b8dd10d0
Anders Børjesson <anbo@zealand.dk>
2021-08-25 12:29:58 +0200

    Add project files.

---
+++ TcpEchoServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpEchoServer
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("This is the server");
+            TcpListener listener = new TcpListener(IPAddress.Loopback, 7);
+            listener.Start();
+            while (true)
+            {
+                Console.WriteLine("Server ready");
+                TcpClient socket = listener.AcceptTcpClient();
+                Console.WriteLine("Incoming client");
+                DoClient(socket);
+            }
+        }
+
+        private static void DoClient(TcpClient socket)
+        {
+            NetworkStream ns = socket.GetStream();
+            StreamReader reader = new StreamReader(ns);
+            StreamWriter writer = new StreamWriter(ns);
+            string message = reader.ReadLine();
+            Console.WriteLine("Server received: " + message);
+            writer.Write(message);
+            writer.Flush();
+            socket.Close();
+        }
+    }
+}
